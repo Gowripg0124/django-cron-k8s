c8a324ac6a89289fffb10559334d0dd4bf6af2c6
Gowri PG <gowri_i4dmarf@MacBook-Air.local>
2026-01-05 19:16:06 +0530

    Fixed Cron job issue

--- core/cron.py
+++ core/cron.py
@@ -1,4 +1,4 @@
 from datetime import datetime
 
 def my_cron_job():
-    print(f"[DJANGO CRON] Job executed at {datetime.now()}")
+    print(f"[DJANGO CRON] Job ran at {datetime.now()}", flush=True)
